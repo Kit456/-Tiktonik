9581c863b64a933ece0b067c1c131b3d34be1684
Kit456 <kirillberreza3344@gmail.com>
2026-03-24 12:34:34 +0500

    Add language support for Russian and English

---
+++ language.lua
@@ -0,0 +1,12 @@
+return {
+    RU = {
+        menu_teleport = "「 Телепорт 」",
+        menu_transport = "「 Транспорт 」",
+        menu_character = "「 Персонаж 」",
+    },
+    EN = {
+        menu_teleport = "「 Teleport 」",
+        menu_transport = "「 Transport 」",
+        menu_character = "「 Character 」",
+    }
+}
