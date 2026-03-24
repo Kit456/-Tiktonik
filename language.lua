cd252ffb4e3801b541b003a625b444f1d34b545c
Kit456 <kirillberreza3344@gmail.com>
2026-03-24 15:06:57 +0500

    Add files via upload

--- language.lua
+++ language.lua
@@ -3,10 +3,19 @@ return {
         menu_teleport = "「 Телепорт 」",
         menu_transport = "「 Транспорт 」",
         menu_character = "「 Персонаж 」",
+        menu_gan = "「 Оружие 」",
+        menu_bots = "「 Боты 」",
+        menu_exit = "「 Выход 」", 
+        prompt_choose_func = "Выберите функцию:", --использовать везде вместо nill
     },
     EN = {
         menu_teleport = "「 Teleport 」",
         menu_transport = "「 Transport 」",
         menu_character = "「 Character 」",
+        menu_gan = "「 Weapons 」",
+        menu_bots = "「 Bots 」",
+        menu_exit = "「 Exit 」", 
+        prompt_choose_func = "Select a function:", --использовать везде вместо nill
     }
 }
+
